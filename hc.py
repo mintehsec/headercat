07b3c759f8ddff6f9ed98a51180b9c73b2fee6d4
MinTeH <mintehsec@users.noreply.github.com>
2014-06-23 20:42:17 +0100

    Create hc.py
    First version

---
+++ hc.py
@@ -0,0 +1,34 @@
+import urllib2; 
+import re
+url = raw_input("Enter URL: ")
+ 
+print """
+  _    _                _              _____      _   
+ | |  | |              | |            / ____|    | |  
+ | |__| | ___  __ _  __| | ___ _ __  | |     __ _| |_ 
+ |  __  |/ _ \/ _` |/ _` |/ _ \ '__| | |    / _` | __|
+ | |  | |  __/ (_| | (_| |  __/ |    | |___| (_| | |_ 
+ |_|  |_|\___|\__,_|\__,_|\___|_|     \_____\__,_|\__|
+------------------------------------------------------                                                     
+                                                      
+ 
+"""
+ 
+response = urllib2.build_opener(urllib2.HTTPSHandler).open(urllib2.Request(url))
+headers = str(response.info())
+print url
+ 
+find = re.search("(Powered.*)", headers)
+if find:
+    # This is reached.
+    print"Found: ", find.group(1)
+ 
+find = re.search("(Asp.*)", headers)
+if find:
+    # This is reached.
+    print"Found: ", find.group(1)
+                
+find = re.search("(IIS.*)", headers)
+if find:
+    # This is reached.
+    print"Found: ", find.group(1)
